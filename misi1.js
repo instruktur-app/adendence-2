2bcc571aa4129530ef9c11a3a14705a6f4ae1571
herzinanda <herzinanda31@gmail.com>
2022-10-15 20:12:20 +0700

    jawaban misi1

--- misi1.js
+++ misi1.js
@@ -1,21 +1,49 @@
 function atendence(arr) {
-    let result = {}
-    // kerjakan disini
-    let sumhadir = 0;
-    let sumsakit = 0;
-
-    for(let i = 0;i < arr.length; i++){
-        if(arr[i] == "Hadir"){
-            sumhadir += 1;
-        }
-        else if (arr[i] == "Sakit"){
-            sumsakit += 1;
-        }
+  let result = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof result[arr[i]] !== "undefined") {
+      result[arr[i]] += 1;
+    } else {
+      result[arr[i]] = 1;
     }
-    result.Hadir = sumhadir;
-    result.Sakit = sumsakit
-    return result;
+  }
+  return result;
 }
 
-console.log(atendence(['Hadir', 'Sakit', 'Hadir', 'Sakit', 'Hadir', 'Sakit', 'Hadir', 'Sakit', 'Hadir', 'Sakit']));
+console.log(
+  atendence([
+    "Hadir",
+    "Sakit",
+    "Hadir",
+    "Sakit",
+    "Hadir",
+    "Sakit",
+    "Hadir",
+    "Sakit",
+    "Hadir",
+    "Sakit",
+  ])
+);
+//{ Hadir: 5, Sakit: 5 }
+
+// function atendence(arr) {
+//     let result = {}
+//     // kerjakan disini
+//     let sumhadir = 0;
+//     let sumsakit = 0;
+
+//     for(let i = 0;i < arr.length; i++){
+//         if(arr[i] == "Hadir"){
+//             sumhadir += 1;
+//         }
+//         else if (arr[i] == "Sakit"){
+//             sumsakit += 1;
+//         }
+//     }
+//     result.Hadir = sumhadir;
+//     result.Sakit = sumsakit
+//     return result;
+// }
+
+// console.log(atendence(['Hadir', 'Sakit', 'Hadir', 'Sakit', 'Hadir', 'Sakit', 'Hadir', 'Sakit', 'Hadir', 'Sakit']));
 //{ Hadir: 5, Sakit: 5 }
